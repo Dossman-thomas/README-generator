39f6e9ccaba603d7d36ac4433ddf192eed0b9c55
Tom Dossman <tdossman.development@gmail.com>
2023-11-21 13:32:07 -0500

    Installed dependencies

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application:
+
+// Import generateMarkdown.js
 const generateMarkdown = require('./utils/generateMarkdown');
+// Import inquirer
+const inquirer = require('inquirer');
+// Import fs
+const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+
+}
 
 
 // TODO: Create a function to initialize app
 function init() {
-  // .. ask the questions here??
+
+  // ask the questions here
+  inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
   let responses = {
     title: "Turtle"
   }
+
   console.log(generateMarkdown(responses));
   
 }
